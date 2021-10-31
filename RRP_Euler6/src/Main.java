5f0775860de9bee7b188d3827a903658f53a7a3b
Ryan Peabody <rpeabody43@gmail.com>
2021-10-30 20:35:20 -0400

    Problem 6

---
+++ RRP_Euler6/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int sqrSum = 0;
+
+        for (int i = 1; i < 101; i++) {
+            sum += i;
+            sqrSum += Math.pow(i, 2);
+        }
+        sum = (int) Math.pow(sum, 2);
+        System.out.println(sum + " - " + sqrSum + " =\n" + (sum - sqrSum));
+    }
+}
