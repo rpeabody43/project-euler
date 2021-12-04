ff8d292562ee82dcc3925e95e3e3bee2e95e2111
Ryan Peabody <rpeabody43@gmail.com>
2021-12-04 01:10:07 -0500

    Problem 71

--- RRP_Euler71/src/Main.java
+++ RRP_Euler71/src/Main.java
@@ -1,30 +1,26 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class Main {
 // Ordered fractions
-
-    static boolean containsFraction (ArrayList<Fraction> arrayList, Fraction fraction) {
-        return arrayList.stream().anyMatch(f -> f.getDecimal() == fraction.getDecimal());
-    }
 
     public static void main(String[] args) {
         // fraction class with numerator, denominator, and decimal form
         // arraylist with every reduced proper fraction where d < 1,000,000
         int range = 1000000;
         double max = (double) 3 / 7;
-        double min = (double) 2 / 5;
-        ArrayList<Fraction> fractionList = new ArrayList<>();
+
+        int n = 2;
+        int d = 5;
+        double result = (double) n / d;
         for (int i = 2; i < range; i++) {
-            if (i % 1000 == 0) System.out.println(i);
-            for (int j = 1; j < i; j++) {
-                if ((double) j / i > min && (double) j / i < max && !containsFraction(fractionList, new Fraction(j, i))) {
-                    fractionList.add(new Fraction(j, i));
+            for (int j = (int) (i*result); j < (int) (i*max); j++) {
+                double x = (double) j / i;
+                if (x < max && x > result) {
+                    result = x;
+                    n = j;
+                    d = i;
                 }
             }
         }
-        fractionList.sort(Comparator.comparing(Fraction::getDecimal));
-        System.out.println(fractionList.get(fractionList.size() - 1));
+        System.out.println(n + "/" + d);
     }
 }
