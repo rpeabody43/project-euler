25e49011ee74756ef74646505ba817df3b0e6b66
Ryan Peabody <rpeabody43@gmail.com>
2022-01-12 17:54:23 -0500

    Problem 14

--- 14/RRP_Euler14/src/Euler14.java
+++ 14/RRP_Euler14/src/Euler14.java
@@ -1,28 +1,18 @@
-import java.util.HashMap;
-
 public class Euler14 {
 
     public static void main (String[] args) {
         long startTime = System.currentTimeMillis();
 
-//        int range = 1000000;
-//        System.out.println(new Euler14().run(range));
-        int test = testLen(910107);
-        System.out.println();
-        System.out.println(test);
+        int range = 1000000;
+        System.out.println(new Euler14().run(range));
 
         double totalTimeSeconds = (System.currentTimeMillis() - startTime) / 1000.0;
         System.out.println(totalTimeSeconds + "s");
     }
 
-    private final HashMap<Integer, Integer> lens;
-
-    public Euler14 () {
-        lens = new HashMap<>();
-    }
-
-    public static int testLen (int num) {
-        System.out.print(num + " ");
+    /* test duplicate function
+    public static int testLen (long num) {
+        System.out.print(num + " -> ");
         if (num <= 1) return 1;
 
         int ret = 1;
@@ -32,16 +22,15 @@ public class Euler14 {
 
         return ret;
     }
+ */
 
-    private int len (int num) {
-//        if (lens.containsKey(num)) return lens.get(num);
-        if (num <= 1) return 0;
+    private int len (long num) {
+        if (num <= 1) return 1;
 
         int ret = 0;
         if (num % 2 == 0)
             ret = len(num/2) + 1;
-        else ret = len(3*num + 1) + 1;
-//        lens.put(num, ret);
+        else ret = len(3*num + 1) + 1;;
         return ret;
     }
 
@@ -49,14 +38,13 @@ public class Euler14 {
         int currentMax = 2;
         int currentMaxLen = 0;
 
-        for (int i = 1; i < range; i++) {
+        for (int i = 0; i < range; i++) {
             int len = len(i);
             if (len > currentMaxLen) {
                 currentMaxLen = len;
                 currentMax = i;
             }
         }
-//        System.out.println(lens);
         return currentMax + " with a length of " + currentMaxLen;
     }
 }
--- 14/is_java_the_issue.py
+++ 14/is_java_the_issue.py
@@ -1,11 +1,30 @@
+# so this works in python but not java
+# I'm so confused
+
+# it was because java ints are tiny
+# this is much slower than java now but I'm not debugging it you can't make me 
+#   (13.23s vs 0.33s)
+
+from time import time
+
+def main():
+    start_time = time()
+    
+    print(run(1000000)) 
+    # print(leng(837799))
+
+    print(str(round(time() - start_time, 2)) + 's')
 
 def leng (num: int) -> int:
     if num == 1: return 1
-    ret: int
-    if num % 2 == 0:
-        ret = leng(num/2) + 1
-    else:
-        ret = leng(3*num + 1) + 1
+    temp = num
+    ret = 1
+    while (temp > 1):
+        if temp % 2 == 0:
+            temp = temp/2
+        else:
+            temp = 3*temp + 1
+        ret += 1
     return ret
 
 def run (max: int) -> str:
@@ -19,7 +38,5 @@ def run (max: int) -> str:
             current_max_len = length
     return str(current_max) + ' with a length of ' + str(current_max_len)
 
-def main():
-    print(run(100)) 
 if __name__  == '__main__':
     main()
